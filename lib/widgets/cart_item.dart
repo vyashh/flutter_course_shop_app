89e3626e63557c1f6ae629f0cf34d985d16fd85e
unknown <vyashbhawan@gmail.com>
2020-10-12 17:25:03 +0200

    added show dialog when removing item from cart

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -29,6 +29,27 @@ class CartItem extends StatelessWidget {
         margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove item from cart?'),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(false);
+                  },
+                  child: Text('No')),
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(true);
+                  },
+                  child: Text('Yes')),
+            ],
+          ),
+        );
+      },
       onDismissed: (direction) {
         // swipe action gevolg
         Provider.of<Cart>(context, listen: false).removeItem(productId);
